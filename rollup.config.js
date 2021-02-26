057744da52cf162c012c8e327952ba4b80ddbfce
cvzi <cuzi@openmail.cc>
2021-02-26 21:41:29 +0100

    Fix: @rollup/plugin-replace: 'preventAssignment' currently defaults to false. It is recommended to set this option to `true`, as the next major version will default this option to `true`.

--- rollup.config.js
+++ rollup.config.js
@@ -69,10 +69,12 @@ export default {
   },
   plugins: [
     replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true
     }),
     replace({
-      ENVIRONMENT: JSON.stringify('production')
+      ENVIRONMENT: JSON.stringify('production'),
+      preventAssignment: true
     }),
     importText(importTextOptions),
     nodeResolve({ extensions: ['.js', '.ts', '.tsx'] }),
