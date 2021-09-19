d6051a5419fa563d61f7a2403a3e0aec26d5ad86
cvzi <cuzi@openmail.cc>
2021-09-19 14:54:12 +0200

    Fix play from search results #19

--- src/index.js
+++ src/index.js
@@ -5423,10 +5423,10 @@ function onLoaded () {
       addStyle('#indexpage .ipCellImage { position:relative }')
     }
 
-    if (document.querySelector('.search .result-items .searchresult.album img,.search .result-items .searchresult.track img')) {
+    if (document.querySelector('.search .result-items .searchresult img')) {
       // Search result pages. To make them compatible, let's add the class names from the discography page
       document.querySelector('.search .result-items').classList.add('music-grid')
-      document.querySelectorAll('.search .result-items .searchresult.album,.search .result-items .searchresult.track').forEach(cell => cell.classList.add('music-grid-item'))
+      document.querySelectorAll(".search .result-items .searchresult[data-search*='\"type\":\"a\"'],.search .result-items .searchresult[data-search*='\"type\":\"t\"']").forEach(cell => cell.classList.add('music-grid-item'))
     }
 
     if (allFeatures.discographyplayer.enabled && document.querySelector('.music-grid .music-grid-item a[href*="/album/"] img,.music-grid .music-grid-item a[href*="/track/"] img')) {
