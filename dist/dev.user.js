0aa0dfb4501d8a6f7d8d682de174dc68ab450189
cvzi <cuzi@openmail.cc>
2024-06-21 12:58:26 +0200

    build

--- dist/dev.user.js
+++ dist/dev.user.js
@@ -21,7 +21,7 @@
 // @connect         *.bcbits.com
 // @connect         genius.com
 // @connect         *
-// @version         1.36.1
+// @version         1.37.0
 // @homepage        https://github.com/cvzi/Bandcamp-script-deluxe-edition
 // @author          cuzi
 // @license         MIT
