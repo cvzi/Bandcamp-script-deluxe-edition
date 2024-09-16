db50c3e50f84c0ef5087f787473324b7b9bb88bd
cvzi <cuzi@openmail.cc>
2024-09-16 11:29:21 +0200

    Update dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -5,7 +5,7 @@ module.exports = {
     ]
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-class-properties',
     '@babel/plugin-syntax-import-assertions'
   ]
 }
